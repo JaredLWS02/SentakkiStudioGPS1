e049f51b12a8bbc403dbd151ac4834f14e3df564
JaredLWS <0135860@kdu-online.com>
2023-09-20 23:40:21 +0800

    Swap System
    Swap System implemented, follows character position, switch on tab

--- SentakkiGame/Assets/SwitchMechanic.cs
+++ SentakkiGame/Assets/SwitchMechanic.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class SwitchMechanic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject CharPos;
+    public GameObject Player1;
+    public GameObject Player2;
+    public PlayerController player1Control;
+    public PlayerController player2Control;
+    public SpriteRenderer P1Sprite;
+    public SpriteRenderer P2Sprite;
+    public bool player1Active = true;
+
     void Start()
     {
-        
+        P1Sprite.enabled = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(player1Active)
+        {
+            CharPos.transform.position = Player1.transform.position;
+        }
+        else
+        {
+            CharPos.transform.position = Player2.transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchPlayer();
+        }
+    }
+
+    public void SwitchPlayer()
+    {
+        if (player1Active)
+        {
+            Player1.SetActive(false);
+            player1Control.enabled = false;
+            P1Sprite.enabled = false;
+            Player2.SetActive(true);
+            Player2.transform.position = CharPos.transform.position;
+            player2Control.enabled = true;
+            P2Sprite.enabled = true;
+            player1Active = false;
+        }
+        else
+        {
+            Player1.SetActive(true);
+            Player1.transform.position = CharPos.transform.position;
+            player1Control.enabled = true;
+            P1Sprite.enabled = true;
+            Player2.SetActive(false);
+            player2Control.enabled = false;
+            P2Sprite.enabled = false;
+            player1Active = true;
+        }
     }
 }
