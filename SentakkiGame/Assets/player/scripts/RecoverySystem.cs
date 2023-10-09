312418570e3bf7b7f29889bd79c5f825627351fb
nyak2 <kaynchan123@gmail.com>
2023-10-09 16:52:05 +0800

    change the animations and  rearrange some files again

---
+++ SentakkiGame/Assets/player/scripts/RecoverySystem.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class RecoverySystem : MonoBehaviour
+{
+    public float hpRestore;
+    public float gaugePointRestore;
+    public healthPoint HealthBar;
+    public GaugePoint gaugePoint;
+
+private void OnTriggerEnter2D(Collider2D col)
+{
+    if (col.CompareTag("Player"))
+    {
+        if (HealthBar.healthAmount < HealthBar.maxHealthAmount)
+        {
+            Destroy(gameObject);
+            HealthBar.RestoreHealthPoints(hpRestore);
+        }
+
+        if (gaugePoint.gaugePointAmount < gaugePoint.maxGaugePointAmount)
+        {
+                Destroy(gameObject);
+            gaugePoint.RestoreGaugePoints(gaugePointRestore);
+        }
+    }
+}
+}
