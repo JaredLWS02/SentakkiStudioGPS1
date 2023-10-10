6ac7c12644ece12c1c8c093cca82ff57d9045e27
nyak2 <kaynchan123@gmail.com>
2023-10-11 02:03:39 +0800

    a

--- SentakkiGame/Assets/players/Rock player/scripts/skill and ultimate.cs
+++ SentakkiGame/Assets/players/Rock player/scripts/skill and ultimate.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class skillandultimate : MonoBehaviour
