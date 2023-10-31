5940bff68a2c745be840b80554721c232d3e284d
JaredLWS <0135860@kdu-online.com>
2023-10-31 21:10:55 +0800

    fixed boss

--- SentakkiGame/Assets/Boss/scripts/Atk1.cs
+++ SentakkiGame/Assets/Boss/scripts/Atk1.cs
@@ -28,40 +28,64 @@ public class Atk1 : MonoBehaviour
 
     private IEnumerator AOE1()
     {
-        yield return new WaitForSecondsRealtime(3);
+        yield return new WaitForSecondsRealtime(3);//3
         atk1Warning[0].SetActive(false);
         atk1[0].SetActive(true);
 
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(1);//4
         atk1Warning[1].SetActive(true);
         atk1Warning[2].SetActive(true);
 
-        yield return new WaitForSecondsRealtime(3);
-        atk1Warning[1].SetActive(false);
-        atk1Warning[2].SetActive(false);
+        yield return new WaitForSecondsRealtime(3);//7
         atk1[0].SetActive(false);
 
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(1);//8
+        atk1Warning[1].SetActive(false);
+        atk1Warning[2].SetActive(false);
         atk1[1].SetActive(true);
         atk1[2].SetActive(true);
 
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(1);//9
         atk1Warning[3].SetActive(true);
         atk1Warning[4].SetActive(true);
 
-        yield return new WaitForSecondsRealtime(3);
-        atk1Warning[3].SetActive(false);
-        atk1Warning[4].SetActive(false);
+        yield return new WaitForSecondsRealtime(3);//12
         atk1[1].SetActive(false);
         atk1[2].SetActive(false);
 
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(1);//13
+        atk1Warning[3].SetActive(false);
+        atk1Warning[4].SetActive(false);
         atk1[3].SetActive(true);
         atk1[4].SetActive(true);
 
-        yield return new WaitForSecondsRealtime(3);
+        yield return new WaitForSecondsRealtime(1);//14
+        atk1Warning[1].SetActive(true);
+        atk1Warning[2].SetActive(true);
+
+        yield return new WaitForSecondsRealtime(3);//17
         atk1[3].SetActive(false);
         atk1[4].SetActive(false);
+
+        yield return new WaitForSecondsRealtime(1);//18
+        atk1Warning[1].SetActive(false);
+        atk1Warning[2].SetActive(false);
+        atk1[1].SetActive(true);
+        atk1[2].SetActive(true);
+
+        yield return new WaitForSecondsRealtime(1);//19
+        atk1Warning[0].SetActive(true);
+
+        yield return new WaitForSecondsRealtime(3);//22
+        atk1[1].SetActive(false);
+        atk1[2].SetActive(false);
+
+        yield return new WaitForSecondsRealtime(1);//23
+        atk1Warning[0].SetActive(false);
+        atk1[0].SetActive(true);
+
+        yield return new WaitForSecondsRealtime(3);//26
+        atk1[0].SetActive(false);
 
         yield return new WaitForSecondsRealtime(15);
         col2d.enabled = true;
