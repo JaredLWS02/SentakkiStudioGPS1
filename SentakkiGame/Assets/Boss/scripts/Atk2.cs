d5852a75933178337c99017ad78c7d244614d9dd
JaredLWS <0135860@kdu-online.com>
2023-11-03 00:20:38 +0800

    started atk2

---
+++ SentakkiGame/Assets/Boss/scripts/Atk2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atk2 : MonoBehaviour
+{
+    [SerializeField] GameObject meteor;
+    [SerializeField] GameObject Target;
+    private Vector2 targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void atk2()
+    {
+
+    }
+
+    private IEnumerator meator()
+    {
+
+    }
+}
